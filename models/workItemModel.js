720532e304c674ac35a7c4c244073e23c08849ad
odooviewsa <erp@view.sa>
2025-02-18 10:41:21 +0200

    Delete models directory

--- models/workItemModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const WorkItemSchema = new mongoose.Schema({
-  workItemName: { type: String, required: true },
-  workDetails: { type: Object },
-  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-});
-
-module.exports = mongoose.model("Work", WorkItemSchema);
